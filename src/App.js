8bc7bde5bc179e48d8a807a3797b20d3304eb3d4
Om Gautam <lionboss1010@gmail.com>
2022-11-06 09:29:15 -0500

    Changed text in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Code-I-Con
+          This is the Code-I-Con Website.
         </p>
         <a
           className="App-link"
